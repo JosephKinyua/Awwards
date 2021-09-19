8e5c38fb6f87e5443f8febef351dd008d5c27415
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-19 22:19:47 +0300

    add the project fields"

--- portfolios/models.py
+++ portfolios/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Projects(models.Model):
+    title = models.CharField(max_length=200)
+    image = CloudinaryField('project/', null=True, blank=True)
+    projectowner = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = HTMLField(null=True, blank=True)
+    livelink = models.URLField(null=True, blank=True)
