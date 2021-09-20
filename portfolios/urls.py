a54e36fd01f4042e5be31a65b10a7927ec375bcc
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-20 15:58:39 +0300

    add a the url path for the projectdetails ans userprofile

--- portfolios/urls.py
+++ portfolios/urls.py
@@ -5,4 +5,10 @@ urlpatterns=[
     path('',views.home,name = 'welcome'),
     path('account/profile', views.profile, name='profile'),
     path('account/postproject', views.postpoject, name='postproject'),
+    path('userprofile/<int:id>', views.userprofile, name='userprofile'),
+    path('projectdetails/<int:id>/', views.projectdetails, name='projectdetails'),
+
+
+
+    
 ] 
