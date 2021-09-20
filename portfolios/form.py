a26724956597fecd4cb745310338126854876345
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-20 10:04:19 +0300

    create  imports for the forms models

--- portfolios/form.py
+++ portfolios/form.py
@@ -1,3 +1,9 @@
+from django import forms
+from django.db.models.fields import TextField
+from django.forms.fields import ChoiceField
+from django.forms.widgets import TextInput
+from .models import Projects, Profile
+from django.contrib.auth.models import User
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
