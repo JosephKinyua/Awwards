ef7f05b88bcfe44b10724c4c43cc2fd9e88adfb4
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-20 04:20:18 +0300

    add the functionality for delete,save and update the projects model

--- portfolios/models.py
+++ portfolios/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from tinymce.models import HTMLField
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Projects(models.Model):
@@ -9,3 +10,17 @@ class Projects(models.Model):
     projectowner = models.ForeignKey(User, on_delete=models.CASCADE)
     description = HTMLField(null=True, blank=True)
     livelink = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        self.save()
+
+    @classmethod
+    def delete_project(cls, id):
+        cls.objects.filter(id=id).delete()
+
+    @classmethod
+    def update_description(cls, id, description):
+        cls.objects.filter(id=id).update(description=description)
