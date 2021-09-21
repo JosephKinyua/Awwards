62300747e9aa8f5d8131f6a94182b3779a54c40a
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-21 10:51:47 +0300

    update of serch urls

--- portfolios/urls.py
+++ portfolios/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns=[
-    path('',views.home,name = 'welcome'),
+    path('',views.home,name = 'home'),
     path('account/profile', views.profile, name='profile'),
     path('account/postproject', views.postpoject, name='postproject'),
     path('userprofile/<int:id>', views.userprofile, name='userprofile'),
     path('projectdetails/<int:id>/', views.projectdetails, name='projectdetails'), 
+    path('search/', views.search, name='search'),
+    path('tinymce/', include('tinymce.urls')),
 ] 
