5b9a419c34528d290883aa22b898754eea6f80aa
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-21 11:51:06 +0300

    update files

--- portfolios/urls.py
+++ portfolios/urls.py
@@ -2,8 +2,8 @@ from django.urls import path,include
 from . import views
 
 urlpatterns=[
-    path('',views.home,name = 'home'),
-    path('account/profile', views.profile, name='profile'),
+    path('home',views.home,name = 'home'),
+    path('', views.profile, name='profile'),
     path('account/postproject', views.postpoject, name='postproject'),
     path('userprofile/<int:id>', views.userprofile, name='userprofile'),
     path('projectdetails/<int:id>/', views.projectdetails, name='projectdetails'), 
